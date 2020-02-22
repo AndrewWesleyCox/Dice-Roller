80e196e6ae7833faee54c03ff100f6e1dd28d9a9
AndrewWesleyCox <andrewwesleycox@gmail.com>
2020-02-22 00:50:33 -0600

    Table formatted

--- Dice.js
+++ Dice.js
@@ -3,27 +3,18 @@ var paragraphs = ["twoP","threeP","fourP","fiveP","sixP","sevenP","eightP","nine
 
 var rollCount = 0;
 
-function thisfunction(){
-    console.log("hello")
-}
-
 function deez(nutz){
     rollCount++;
     numbers[nutz]++;
     changePercentages();
 }
 
-function loadNumbers(){
-    var x;
-    for(x = 0;x < 11;x++){
-        document.getElementById(paragraphs[x]).innerHTML = numbers[x] + " - 0.00%";
-    }
-}
-
 function changePercentages(){
+    document.getElementById("total").innerHTML = rollCount;
     var x;
     for(x = 0; x < 11;x++){
-        var percent = (numbers[x]/rollCount)*100;
+        if(rollCount == 0) percent = 0;
+        else var percent = (numbers[x]/rollCount)*100;
         document.getElementById(paragraphs[x]).innerHTML = numbers[x] + " - " + percent.toFixed(2) + "%";
     }
 }
