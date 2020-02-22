c246d76aa8285960c201f23bf600c3d006de0d16
AndrewWesleyCox <andrewwesleycox@gmail.com>
2020-02-22 00:20:28 -0600

    Percentages working

--- Dice.js
+++ Dice.js
@@ -1,18 +1,29 @@
 var numbers = [0,0,0,0,0,0,0,0,0,0,0];
 var paragraphs = ["twoP","threeP","fourP","fiveP","sixP","sevenP","eightP","nineP","tenP","elevenP","twelveP"];
 
+var rollCount = 0;
+
 function thisfunction(){
     console.log("hello")
 }
 
 function deez(nutz){
+    rollCount++;
     numbers[nutz]++;
-    document.getElementById(paragraphs[nutz]).innerHTML = numbers[nutz]
+    changePercentages();
 }
 
 function loadNumbers(){
     var x;
-    for(x = 0;x < 10;x++){
-        document.getElementById(paragraphs[x]).innerHTML = numbers[x];
+    for(x = 0;x < 11;x++){
+        document.getElementById(paragraphs[x]).innerHTML = numbers[x] + " - 0.00%";
+    }
+}
+
+function changePercentages(){
+    var x;
+    for(x = 0; x < 11;x++){
+        var percent = (numbers[x]/rollCount)*100;
+        document.getElementById(paragraphs[x]).innerHTML = numbers[x] + " - " + percent.toFixed(2) + "%";
     }
 }
